41bf97a3bb7a61ba7d4a50cf67e0b41e229651b4
mozammel787 <mhfaryanemon@gmail.com>
2024-02-25 14:52:30 +0600

    If Else

--- Looping.dart
+++ Looping.dart
@@ -1,9 +1,13 @@
 main()
 {
   for (int i=0; i <= 100; i= i+1 ) {
+    //Continue deya 0 k bad deya 1 theka count start kora jai//
+
     if (i ==0){
       continue;
     }
+    //Break deya specific kono number e break kora jai//
+
     if (i == 50) {
       break;
     }
