23e4f55c7ddd866677fa34e6b2eae5378d4f682c
MdShaheImran <mdshaheimran54@gmail.com>
2024-02-27 16:35:21 +0600

    Looping Updated

--- Looping.dart
+++ Looping.dart
@@ -1,6 +1,6 @@
 main()
 {
-  for (int i=0; i <= 100; i= i+1 ) {
+ /* for (int i=0; i <= 100; i= i+1 ) {
     //Continue deya 0 k bad deya 1 theka count start kora jai//
 
     if (i ==0){
@@ -13,5 +13,16 @@ main()
     }
     print("Office e jabo $i ");
   }
-  print("Loop is done");
+  print("Loop is done");*/
+
+  List<String> studentList = ['Imran','Maruf', 'Kamal' ];
+  for( int i=0; i <studentList.length; i++) {
+    print("studentList $i=> ${studentList[i]}");
+  }
+
+  //For Each
+  for (String item in studentList) {
+    print(item);
+  }
+
 }
